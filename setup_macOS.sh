095ffad18ee1dbca144094350f742e22c5c02c55
XuehaiPan <XuehaiPan@pku.edu.cn>
2019-11-04 09:18:31 +0800

    style: format and cleanup code

--- setup_macOS.sh
+++ setup_macOS.sh
@@ -533,15 +533,15 @@ export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bot
 # Anaconda
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="\$('\$HOME/Miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="\$('\$HOME/Miniconda3/bin/conda' 'shell.bash' 'hook' 2>/dev/null)"
 if [ \$? -eq 0 ]; then
-    eval "\$__conda_setup"
+	eval "\$__conda_setup"
 else
-    if [ -f "\$HOME/Miniconda3/etc/profile.d/conda.sh" ]; then
-        . "\$HOME/Miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="\$HOME/Miniconda3/bin:\$PATH"
-    fi
+	if [ -f "\$HOME/Miniconda3/etc/profile.d/conda.sh" ]; then
+		. "\$HOME/Miniconda3/etc/profile.d/conda.sh"
+	else
+		export PATH="\$HOME/Miniconda3/bin:\$PATH"
+	fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<

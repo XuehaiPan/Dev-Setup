7e687242c8d31c678314c52dbade3b98f325f604
XuehaiPan <XuehaiPan@pku.edu.cn>
2020-01-29 22:33:35 +0800

    chore: install package Term::ReadLine::Perl Term::ReadKey for perl

--- setup_macOS.sh
+++ setup_macOS.sh
@@ -264,6 +264,7 @@ echo_and_eval 'cpan -i local::lib'
 echo_and_eval 'eval "$(perl -I/usr/local/opt/perl/lib/perl5 -Mlocal::lib=/usr/local/opt/perl)"'
 echo_and_eval 'cpan -i CPAN'
 echo_and_eval 'cpan -i Log::Log4perl'
+echo_and_eval 'yes exit | cpan -i Term::ReadLine::Perl Term::ReadKey'
 
 # Configurations for Zsh
 backup_dotfiles .dotfiles/.zshrc-common

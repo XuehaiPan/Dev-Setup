70207d256c92c42cff7a5e906c42068db6d56d40
XuehaiPan <XuehaiPan@pku.edu.cn>
2020-02-05 20:34:15 +0800

    style: add double quotes marks in strings

--- setup_Manjaro.sh
+++ setup_Manjaro.sh
@@ -1637,8 +1637,8 @@ echo_and_eval 'conda install pip jupyter ipython notebook jupyterlab ipdb \
 							 tqdm yapf autopep8 pycodestyle pylint --yes'
 echo_and_eval 'conda update --all --yes'
 echo_and_eval 'conda clean --all --yes'
-echo_and_eval "\$HOME/$CONDA_DIR/bin/jt --theme monokai"
-echo_and_eval "\$HOME/$CONDA_DIR/bin/jupyter contrib nbextension install --user &>/dev/null"
+echo_and_eval "\"\$HOME/$CONDA_DIR/bin/jt\" --theme monokai"
+echo_and_eval "\"\$HOME/$CONDA_DIR/bin/jupyter\" contrib nbextension install --user &>/dev/null"
 rm -r .cph_tmp* 2>/dev/null
 rm -r "$CONDA_DIR"/.cph_tmp* 2>/dev/null
 

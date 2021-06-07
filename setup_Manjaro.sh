2ca51bd69edf47ca00b50aeeac24c9ab7b51916b
XuehaiPan <XuehaiPan@pku.edu.cn>
2021-06-07 21:25:25 +0800

    chore: update settings for diff-so-fancy

--- setup_Manjaro.sh
+++ setup_Manjaro.sh
@@ -232,7 +232,20 @@ git config --global filter.lfs.process 'git-lfs filter-process'
 git config --global filter.lfs.required true
 git config --global color.ui true
 if [[ -x "$(command -v diff-so-fancy)" ]]; then
-	diff-so-fancy --set-defaults
+	git config --global core.pager 'diff-so-fancy | less --tabs=4 -RFX'
+	git config --global interactive.diffFilter 'diff-so-fancy --patch'
+	git config --global color.diff-highlight.oldNormal    'red bold'
+	git config --global color.diff-highlight.oldHighlight 'red bold 52'
+	git config --global color.diff-highlight.newNormal    'green bold'
+	git config --global color.diff-highlight.newHighlight 'green bold 22'
+
+	git config --global color.diff.meta       '11'
+	git config --global color.diff.frag       'magenta bold'
+	git config --global color.diff.func       '146 bold'
+	git config --global color.diff.commit     'yellow bold'
+	git config --global color.diff.old        'red bold'
+	git config --global color.diff.new        'green bold'
+	git config --global color.diff.whitespace 'red reverse'
 fi
 
 mv -f .gitconfig .dotfiles/.gitconfig

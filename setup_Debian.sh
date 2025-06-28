4f163c8557a33c2b4cd48e8b1512c2dff10ed467
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-06-29 03:40:53 +0800

    chore: install zlib and yaml headers

--- setup_Debian.sh
+++ setup_Debian.sh
@@ -305,7 +305,8 @@ if have_sudo_access; then
 	exec_cmd 'sudo chown root:root /usr/local/bin/diff-so-fancy'
 	exec_cmd 'sudo apt-get install htop ssh net-tools atool tree colordiff xclip --yes'
 	exec_cmd 'sudo apt-get install make cmake build-essential gcc g++ gdb --yes'
-	exec_cmd 'sudo apt-get install clang clang-format llvm lldb ruby-full libssl-dev libreadline-dev --yes'
+	exec_cmd 'sudo apt-get install clang clang-format llvm lldb --yes'
+	exec_cmd 'sudo apt-get install ruby-full libssl-dev libreadline-dev libyaml-dev zlib1g-dev --yes'
 	exec_cmd 'sudo apt-get autoremove --purge --yes'
 	exec_cmd 'sudo apt-get autoclean'
 fi

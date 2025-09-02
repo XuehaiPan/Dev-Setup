af84cf6fa4810779245b254cf7ccc4503d3a6763
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-09-02 18:10:59 +0800

    chore: update `PS1` for bash

--- setup_Ubuntu.sh
+++ setup_Ubuntu.sh
@@ -1512,6 +1512,8 @@ export TERM="xterm-256color"
 export LESS="-R -M -i -j5"
 if [[ -f "${HOME}/.dotfiles/gitstatus/gitstatus.prompt.sh" ]]; then
 	GITSTATUS_NUM_THREADS=4 source "${HOME}/.dotfiles/gitstatus/gitstatus.prompt.sh"
+	export PS1="${PS1/@/\\[\\e[0m\\e[1;37m\\]@}"
+	export PS1="${PS1/\\[hH]/\\[\\e[0m\\e[1;36m\\]\\H}"
 elif [[ -n "${SSH_CONNECTION}" ]]; then
 	export PS1='[\[\e[1;33m\]\u\[\e[0m\]@\[\e[1;32m\]\H\[\e[0m\]:\[\e[1;35m\]\w\[\e[0m\]]\$ '
 else
